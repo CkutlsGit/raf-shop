4319cb528d4b49f49817ed98db5e6c2348126d70
Kostya <ckutls228@gmail.com>
2025-04-07 22:17:49 +0300

    added component style for baseinput-search

--- components/base/BaseSearch.vue
+++ components/base/BaseSearch.vue
@@ -1,13 +1,43 @@
+<script lang="ts" setup></script>
+
 <template>
-  <div>
-    
+  <div class="base__content">
+    <input class="base-input" type="text" placeholder="Найти товар" />
+    <img class="base-icon-search" src="/public/icons/search-icon.svg" />
   </div>
 </template>
 
-<script lang="ts" setup>
+<style scoped>
+.base__content {
+  position: relative;
+}
 
-</script>
+.base-input {
+  width: 100%;
+  border: none;
+  border-radius: var(--size-base);
+  margin-top: var(--size-xs);
+  padding: var(--size-base) 50px var(--size-base) var(--size-base);
+  background: var(--bg-color-main);
+  color: var(--text-color-main);
+  font-size: var(--size-big);
+  font-weight: 500;
+  font-family: "Raleway", sans-serif;
+}
 
-<style>
+.base-input::placeholder {
+  font-weight: 600;
+}
 
+.base-input:focus {
+  outline: none;
+}
+
+.base-icon-search {
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
 </style>
