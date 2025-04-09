274a35a53b4671e2b12699b22df9166941127014
Kostya <ckutls228@gmail.com>
2025-04-09 07:09:55 +0300

    added products layout

---
+++ layouts/products/products.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <slot name="header"></slot>
+  <ul class="products__content">
+    <products-item-product></products-item-product>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,11 +7,11 @@
         <base-search></base-search>
         <base-carousel></base-carousel>
       </header>
-      <article class="main-page__categories">
-        <header class="categories__header">
+      <article class="main-page__categories block-style">
+        <header class="categories__header block-header-style">
           <h1>Категории</h1>
           <base-button>
-          <span><img src="/public/icons/arrow-icon.svg"></span>
+            <span><img src="/public/icons/arrow-icon.svg" /></span>
           </base-button>
         </header>
         <ul class="categories__content">
@@ -21,11 +21,15 @@
           <li><category-item-category></category-item-category></li>
         </ul>
       </article>
-      <footer class="main-page__for-you">
-        <header class="for-you__header">
-          <h1>Ваша подборка</h1>
-          <img src="/public/icons/arrow-icon.svg">
-        </header>  
+      <footer class="main-page__for-you block-style">
+        <NuxtLayout name="products">
+          <template #header>
+            <header class="for-you__header block-header-style">
+              <h1>Ваша подборка</h1>
+              <img src="/public/icons/arrow-icon.svg" />
+            </header>
+          </template>
+        </NuxtLayout>
       </footer>
     </div>
   </section>
@@ -40,21 +44,25 @@
   cursor: pointer;
 }
 
-.main-page__categories {
+.block-style {
   width: 100%;
   background: var(--bg-color-main);
   border-radius: var(--size-base);
   margin: var(--size-xs) 0;
 }
 
-.categories__header {
+.for-you__header img {
+  cursor: pointer;
+}
+
+.block-header-style {
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: var(--size-lg) var(--size-small);
 }
 
-.categories__header h1 {
+.block-header-style h1 {
   font-size: var(--size-medium);
   font-weight: 600;
 }
