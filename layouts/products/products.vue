d4cbfa4b398661e52366be6e4ac6e39b1d2e91d4
Kostya <ckutls228@gmail.com>
2025-04-14 20:14:15 +0300

    added layout for for-you page, changed logic for layout products

--- layouts/products/products.vue
+++ layouts/products/products.vue
@@ -2,6 +2,10 @@
 const runTimeConfig = useRuntimeConfig()
 const products = ref<IProduct[]>()
 
+const props = defineProps<{
+  typeLayout: string
+}>()
+
 onMounted(async () => {
   const response = await $fetch<IProduct[]>(
     `${runTimeConfig.public.backendUrl}/api/v1/products`,
@@ -16,7 +20,7 @@ onMounted(async () => {
 
 <template>
   <slot name="header"></slot>
-  <ul class="products__content block-content-style">
+  <ul class="products__content block-content-style" :class="{ 'split-block-style': typeLayout }">
     <products-item-product
       v-for="product in products"
       :key="product.id"
@@ -28,5 +32,12 @@ onMounted(async () => {
 <style scoped>
 .products__content {
   grid-template-columns: repeat(2, minmax(0, 1fr));
+}
+
+.split-block-style {
+  width: 100%;
+  background: var(--bg-color-main);
+  border-radius: var(--size-base);
+  padding: var(--size-small);
 }
 </style>
--- pages/for-you.vue
+++ pages/for-you.vue
@@ -1,13 +1,28 @@
-<template>
-  <div>
-    <NuxtLink to="/">back</NuxtLink>
-  </div>
-</template>
-
 <script lang="ts" setup>
 
 </script>
 
-<style>
+<template>
+  <section class="for-you">
+    <div class="for-you__content">
+      <NuxtLayout name="products" :type-layout="'header-products'">
+          <template #header>
+            <header class="for-you__header block-header-style block-header-img-style">
+              <h1 class="text-title-style">Ваша подборка</h1>
+              <NuxtLink to="/for-you"
+                ><img src="/public/icons/arrow-icon.svg"
+              /></NuxtLink>
+            </header>
+          </template>
+        </NuxtLayout>
+    </div>
+  </section>
+</template>
 
+<style scoped>
+.for-you__header {
+  background: var(--bg-color-main);
+  border-radius: var(--size-base);
+  margin: var(--size-xs) 0;
+}
 </style>
