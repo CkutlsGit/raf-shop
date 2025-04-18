f7198f4c8b29ed358191f8082b420741f47ecb67
Kostya <ckutls228@gmail.com>
2025-04-18 13:17:18 +0300

    added logic and template for category-id, added component for header-unsplit block

--- layouts/categories/subcategories.vue
+++ layouts/categories/subcategories.vue
@@ -1,13 +1,64 @@
+<script setup lang="ts">
+const props = defineProps<{
+  dataCategory: ICategoriesFull
+}>()
+</script>
+
 <template>
-  <div>
-    <slot />
+  <BaseLoader v-if="!dataCategory"></BaseLoader>
+  <div v-else>
+    <article
+      v-if="dataCategory.products.length > 0"
+      class="categories-subcategories__main"
+    >
+      <header-block
+        :name="dataCategory.name"
+        link-back="/categories"
+      ></header-block>
+      <ul class="products__content block-content-style split-block-style">
+        <products-item-product
+          v-for="product in dataCategory.products"
+          :key="product.id"
+          :product="product"
+        ></products-item-product>
+      </ul>
+    </article>
+    <header-block
+      v-else
+      :name="dataCategory.name"
+      link-back="/categories"
+    ></header-block>
+    <article
+      v-for="subCategory in dataCategory.subCategories"
+      :key="subCategory.id"
+      class="categories-subcategories__submain block-style"
+    >
+    <div v-if="subCategory.products.length > 0">
+      <header class="for-you__header block-header-style block-header-img-style">
+        <h1 class="text-title-style">{{ subCategory.name }}</h1>
+        <NuxtLink>
+          <base-button class="icon-arrow">
+            <span><img src="/public/icons/arrow-icon.svg" /></span>
+          </base-button>
+        </NuxtLink>
+      </header>
+      <ul
+        class="products__content block-content-style"
+        v-if="subCategory.products"
+      >
+        <products-item-product
+          v-for="product in subCategory.products"
+          :key="product.id"
+          :product="product"
+        ></products-item-product>
+      </ul>
+    </div>
+    </article>
   </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
+<style scoped>
+.products__content {
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+}
 </style>
--- pages/categories/[id].vue
+++ pages/categories/[id].vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+const runTimeConfig = useRuntimeConfig()
+const route = useRoute()
 
+const dataCategory = ref<ICategoriesFull>()
+
+onMounted(async () => {
+  try {
+    const response = await $fetch<ICategoriesFull>(`${ runTimeConfig.public.backendUrl }/api/v1/products/${ route.params.id }`, {
+      method: 'GET'
+    })
+
+    dataCategory.value = response
+  }
+  catch(error) {
+    console.error(error)
+  }
+})
 </script>
 
 <template>
   <section class="categories-list">
     <div class="categories-list__content">
-
+      <NuxtLayout name="categories-subcategories" :data-category="dataCategory"></NuxtLayout>
     </div>
   </section>
 </template>
