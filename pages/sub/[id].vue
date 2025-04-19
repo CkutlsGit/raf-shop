9ddfd97c311af8eaa719b179159cf28ec5b48333
Kostya <ckutls228@gmail.com>
2025-04-19 19:00:47 +0300

    added logic and template for sub-categories page

---
+++ pages/sub/[id].vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+const runTimeConfig = useRuntimeConfig()
+const route = useRoute()
+
+const dataCategory = ref<ISubCategories>()
+
+onMounted(async () => {
+  try {
+    const response = await $fetch<ICategoriesFull>(
+      `${runTimeConfig.public.backendUrl}/api/v1/products/${route.query.category}`,
+      {
+        method: "GET",
+      }
+    )
+
+    dataCategory.value = response.subCategories.find(
+      (subCategory) => subCategory.id === route.params.id
+    )
+  } catch (error) {
+    console.error(error)
+  }
+})
+</script>
+
+<template>
+  <section class="subcategories-main">
+    <BaseLoader v-if="!dataCategory"></BaseLoader>
+    <div v-else class="sub-categories-main__content">
+      <header-block
+        :name="dataCategory?.name"
+        :link-back="`/categories/${route.query.category}`"
+      ></header-block>
+      <article class="sub-categories__products">
+        <ul
+        class="products__content block-content-style split-block-style" 
+      >
+        <products-item-product
+          v-for="product in dataCategory.products"
+          :key="product.id"
+          :product="product"
+        ></products-item-product>
+      </ul>
+      </article>
+      <!-- todo: подумать над переносом в компонент -->
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.products__content {
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+}
+</style>
