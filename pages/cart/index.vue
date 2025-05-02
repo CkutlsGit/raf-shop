127ed4661a41a6a24cb360011bce768ff45ab31a
Kostya <ckutls228@gmail.com>
2025-05-02 08:25:01 +0300

    added test style for clear-cart in cart page

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -23,10 +23,32 @@ onMounted(async () => {
   <section class="cart">
     <div class="cart__content--clear block-style">
       <HeaderBlockUnsplit name="Корзина" link-back="/"></HeaderBlockUnsplit>
+      <article class="cart__text">
+        <h1>Тут пусто</h1>
+        <p>Наполни корзину товарами из каталога</p>
+        <div class="cart__button">
+          <button>В каталог <span><img src="/public/icons/arrow-icon.svg"></span></button>
+        </div>
+      </article>
     </div>
   </section>
 </template>
 
 <style scoped>
+.cart__content--clear {
+  height: 100%;
+}
 
+.cart__text {
+  margin-top: 60%;
+  text-align: center;
+}
+
+.cart__text p {
+  margin: 2px 0 var(--size-xs) 0;
+}
+
+.cart__button {
+  padding-bottom: 110%;
+}
 </style>
